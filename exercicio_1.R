4994b6fa753c754206e7000a79eeb823a4b94fc6
Emerson Antonio <afirmanet@gmail.com>
2022-09-26 16:26:20 -0300

    add dataset

--- exercicio_1.R
+++ exercicio_1.R
@@ -11,3 +11,18 @@ getwd()
 # Carrega o dataset
 data <- read_csv("dados/CAERS_ASCII_2004_2017Q2.csv")
 
+#Mostre que a tabela foi importada com sucesso
+View(data)
+
+
+# 1. Qual o **tipo de reação** mais reportado na tabela?
+data$`PRI_Product Role`
+
+
+# 2. Qual o **nome da indústria** que tem **mais mortes** reportadas?
+data$`PRI_FDA Industry Name`
+data$`AEC_One Row Outcomes`
+
+# 3. Quais as **3 principais reações adversas** de pessoas entre 18-25 anos na **indústria de cosméticos**?
+# 4. Usando o `purrr`, escreva em uma pasta 10 tabelas diferentes com as **10 reações mais recorrentes** (uma tabela para cada reação).
+
