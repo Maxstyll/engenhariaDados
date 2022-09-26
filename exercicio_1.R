702336763b34b93730bc29cf3f55ceb06c8f49a2
Emerson Antonio <afirmanet@gmail.com>
2022-09-26 16:25:34 -0300

    add dataset

--- exercicio_1.R
+++ exercicio_1.R
@@ -1,6 +1,13 @@
+# Solução do Exercício 1 em R
 
+# Imports
+install.packages("tidyverse")
+library(tidyverse)
 
+# Diretório de trabalho
+setwd("E:/Developement/Estudos/samples/R/engenhariaDados")
+getwd()
 
-#Exercício 1
+# Carrega o dataset
+data <- read_csv("dados/CAERS_ASCII_2004_2017Q2.csv")
 
-"E:\Developement\Estudos\samples\R\engenhariaDados"
