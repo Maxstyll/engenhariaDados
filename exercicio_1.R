87dc8b964ae1cfe42c79f9cd1b74f7cd8bef58d0
Emerson Antonio <afirmanet@gmail.com>
2022-09-27 15:02:54 -0300

    create task 1 a 3

--- exercicio_1.R
+++ exercicio_1.R
@@ -2,7 +2,9 @@
 
 # Imports
 install.packages("tidyverse")
+
 library(tidyverse)
+library(purrr)
 
 # Diretório de trabalho
 setwd("E:/Developement/Estudos/samples/R/engenhariaDados")
@@ -14,15 +16,43 @@ data <- read_csv("dados/CAERS_ASCII_2004_2017Q2.csv")
 #Mostre que a tabela foi importada com sucesso
 View(data)
 
-
 # 1. Qual o **tipo de reação** mais reportado na tabela?
-data$`PRI_Product Role`
+symptoms = data$`SYM_One Row Coded Symptoms`
+
+symptoms.freq = sort(table(unlist(strsplit(symptoms, ", "))), decreasing = TRUE)
+symptoms.freq[1]
 
 
 # 2. Qual o **nome da indústria** que tem **mais mortes** reportadas?
-data$`PRI_FDA Industry Name`
-data$`AEC_One Row Outcomes`
+dataDeth <- filter(data, `AEC_One Row Outcomes` == "DEATH")
+Industry = dataDeth$`PRI_Reported Brand/Product Name`
+Industry.freq = sort(table(Industry), decreasing = TRUE)
+Industry.freq[1]
+
 
 # 3. Quais as **3 principais reações adversas** de pessoas entre 18-25 anos na **indústria de cosméticos**?
-# 4. Usando o `purrr`, escreva em uma pasta 10 tabelas diferentes com as **10 reações mais recorrentes** (uma tabela para cada reação).
+dataAge <- filter(data, `CI_Age Unit` == "Year(s)" & 
+                        `PRI_FDA Industry Name` == "Cosmetics" &
+                        (`CI_Age at Adverse Event` >= 18 & 
+                           `CI_Age at Adverse Event` <= 25))
+symptomsForAge = dataAge$`SYM_One Row Coded Symptoms`
+symptomsForAge.freq = sort(table(unlist(strsplit(symptoms, ", "))), decreasing = TRUE)
+symptomsForAge.freq[1:3]
 
+
+# 4. Usando o `purrr`, escreva em uma pasta 10 tabelas diferentes com as **10 reações mais recorrentes** (uma tabela para cada reação).
+symptomsRecod = data$`SYM_One Row Coded Symptoms`
+symptomsRecod.freq = sort(table(unlist(strsplit(symptoms, ", "))), decreasing = TRUE)
+
+# Step 1
+output_csv <- function(data, names){ 
+  folder_path <- "./datasets/test-csv/"
+  
+  write_csv(data, paste0(folder_path, "test-excel-", names, ".csv"))
+}
+
+symptomsRecod.freq[1:10]
+
+list(data = symptomsRecod.freq,
+     names = names(symptomsRecod.freq)) %>% 
+  purrr::pmap(output_csv) 
